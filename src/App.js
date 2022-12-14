bced6e686fec10b45cd262ad30fe883f60a28bbf
ARESKAN2108 <maxareskan@gmail.com>
2022-12-14 16:49:17 +0300

    222

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-    
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>javascript</li>
+        <li>react</li>
+      </ul>
     </div>
   );
 }
