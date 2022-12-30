e6b545949c027412a070cdf66822618ce87ac891
ARESKAN2108 <maxareskan@gmail.com>
2022-12-30 14:55:34 +0300

    dispatch and action

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,45 +1,58 @@
-import { rerenderEntireThree } from "../render";
-
-let state = {
-    profilePage: {
-        posts: [
-            { id: 1, message: 'Hi, how are you?', likesCount: 15 },
-            { id: 2, message: 'It`s my first post', likesCount: 23 }
-        ],
-        newPostText: 'it-kamasutra.com'
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                { id: 1, message: 'Hi, how are you?', likesCount: 15 },
+                { id: 2, message: 'It`s my first post', likesCount: 23 }
+            ],
+            newPostText: 'it-kamasutra.com'
+        },
+       
+        dialogsPage: {
+            dialogs: [
+                { id: 1, name: 'Andrey' },
+                { id: 2, name: 'Sasha' },
+                { id: 3, name: 'Anton' },
+                { id: 4, name: 'Dima' },
+                { id: 5, name: 'Lina' }
+            ],
+            messages: [
+                { id: 1, message: 'Hello' },
+                { id: 2, message: 'How are you?' },
+                { id: 3, message: 'I`m fine' },
+                { id: 4, message: 'progress' },
+                { id: 5, message: 'my wife' }
+            ]
+        }
     },
-   
-    dialogsPage: {
-        dialogs: [
-            { id: 1, name: 'Andrey' },
-            { id: 2, name: 'Sasha' },
-            { id: 3, name: 'Anton' },
-            { id: 4, name: 'Dima' },
-            { id: 5, name: 'Lina' }
-        ],
-        messages: [
-            { id: 1, message: 'Hello' },
-            { id: 2, message: 'How are you?' },
-            { id: 3, message: 'I`m fine' },
-            { id: 4, message: 'progress' },
-            { id: 5, message: 'my wife' }
-        ]
+    _callSubscriber () {
+        console.log('State changed')
+    },
+
+    getState () {
+        return this._state;
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    },
+
+    dispatch(action) {
+        if(action.type === 'ADD-POST') {
+            let newPost = {
+                id: 5,
+                message: this._state.profilePage.newPostText,
+                likesCount: 0
+            }
+            this._state.profilePage.posts.push(newPost);
+            this._state.profilePage.newPostText = '';
+            this._callSubscriber(this._state);
+        } else if(action.type === 'UPDATE-NEW-POST-TEXT') {
+            this._state.profilePage.newPostText = action.newText;
+            this._callSubscriber(this._state);
+        }
     }
+    
 }
 
-export let addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    }
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireThree(state);
-};
-
-export let updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireThree(state);
-};
-export default state;
+window.store = store;
+export default store;
